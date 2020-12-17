f51b2e39a5cd898e80c8703b85706416db879367
gabel35 <gabrieljose3135@gmail.com>
2020-12-16 22:41:02 -0500

    first commit w/ pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+alert ("work?")
+
+// connect weatherAPI//
+// have search button call forth the information for the city typed//
+// display current day information on the main div//
+//display the next 5 days in their repective card//
+// have the city typed save to the log and appear as a new button //
+// have the button with the city name call the weather stats for that city and display in in the main section//
