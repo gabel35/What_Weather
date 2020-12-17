e6dc6bcca9b37be00f1e0e4be9e0edc93751c261
gabel35 <gabrieljose3135@gmail.com>
2020-12-17 13:16:12 -0500

    connected weather API and tested it

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-alert ("work?")
 
 // connect weatherAPI//
+var APIKey = "9b97bdc0a57667e94c3b8958a0c57307";
+var cityName = "Newark";
+  // Here we are building the URL we need to query the database
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+    "q=" + cityName + "&appid=" + APIKey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+      console.log(queryURL);
+      console.log(response);
+    })
+
 // have search button call forth the information for the city typed//
 // display current day information on the main div//
 //display the next 5 days in their repective card//
 // have the city typed save to the log and appear as a new button //
-// have the button with the city name call the weather stats for that city and display in in the main section//
+// have the button with the city name call the weather stats for that city and display in in the main section
