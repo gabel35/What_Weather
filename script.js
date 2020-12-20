99a82ef2217fec8ef98345d313ee4bc1fac4267b
gabel35 <gabrieljose3135@gmail.com>
2020-12-19 21:48:52 -0500

    got uv index and a better date look

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ $("#magnifying-glass").on("click", function(event) {
     .then(function(response) {
         console.log(queryWeatherNow);
         console.log(response);
-
+        //name of city and date//
         var curDiv = $("#resultDiv");
         var cityName = response.name;
         var dateNow = new Date(response.dt *1000);
@@ -26,26 +26,35 @@ $("#magnifying-glass").on("click", function(event) {
         console.log(cityName);
         var resultCity = $("<h1>").text(cityName + " (" + dateNow + ")");
         curDiv.append(resultCity);
-
+        //current temperature//
         var tempNow = response.main.temp;
         console.log(tempNow);
         var resultTempNow = $("<p>").text("Temperature: " + tempNow + "°F");
         curDiv.append(resultTempNow);
-
+        //current humidity//
         var humNow = response.main.humidity;
         console.log(humNow);
         var resultHumNow = $("<p>").text("Humidity: " + humNow + "%");
         curDiv.append(resultHumNow);
-
+        //current wind speed//
         var windNow = response.wind.speed;
         console.log(windNow);
         var resultWindNow = $("<p>").text("Wind Speed: " + windNow + " MPH");
         curDiv.append(resultWindNow);
-
-        // var uviNow = response.wind.speed;
-        // console.log(uviNow);
-        // var resultUVINow = $("<p>").text("UV Index: " + UVINow);
-        // curDiv.append(resultUVINow);
+        //current UV index//
+        var latItDude = response.coord.lat;
+        var lonGitDude = response.coord.lon;
+        var queryUVI = "http://api.openweathermap.org/data/2.5/uvi?lat="+ latItDude + "&lon=" + lonGitDude + "&appid=" + APIKey;
+        $.ajax({
+            url: queryUVI,
+            method: "GET"
+        })
+        .then(function(response) {
+            var uviNow = response.value;
+            console.log(uviNow);
+            var resultUVINow = $("<p>").text("UV Index: " + uviNow);
+            curDiv.append(resultUVINow);
+        })
     })
 
     var queryForecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&cnt=5&appid=" + APIKey;
